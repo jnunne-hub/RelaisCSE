4ab82dea932ffa4303bf1ed21dca63aacb742f83
jnunne-hub <nunnejulien@gmail.com>
2025-06-14 22:38:35 +0200

    Create firebase-config.js

---
+++ config/firebase-config.js
@@ -0,0 +1,12 @@
+
+// Exemple de configuration Firebase. Copiez ce fichier en
+// config/firebase-config.js et remplissez avec vos valeurs.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAyHUsKhQ9HdUQkizEKEsn37rOUmHuqrk", // VOTRE CLÉ API (gardez-la secrète idéalement via variables d'env. pour projet pro)
+  authDomain: "vinted-entreprise.firebaseapp.com",
+  projectId: "vinted-entreprise",
+  storageBucket: "vinted-entreprise.appspot.com", // Vérifiez ce nom dans votre console Firebase (généralement sans firebase)
+  messagingSenderId: "301194568056",
+  appId: "1:301194568056:web:ef6da7eb1c0795e2eae88d",
+  measurementId: "G-EEGS88XHHB" // Optionnel pour Analytics
+};
