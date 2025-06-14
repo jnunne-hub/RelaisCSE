9c27f0224738eb050a6e02d6e99a7ee780b184d4
jnunne-hub <nunnejulien@gmail.com>
2025-06-14 21:26:15 +0200

    Update firebase-config.js

--- config/firebase-config.example.js
+++ config/firebase-config.example.js
@@ -1,11 +1,11 @@
 // Exemple de configuration Firebase. Copiez ce fichier en
 // config/firebase-config.js et remplissez avec vos valeurs.
 export const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_AUTH_DOMAIN",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_STORAGE_BUCKET",
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-  appId: "VOTRE_APP_ID",
-  measurementId: "VOTRE_MEASUREMENT_ID"
+  apiKey: "AIzaSyCAyHUsKhQ9HdUQkizEKEsn37rOUmHuqrk", // VOTRE CLÉ API (gardez-la secrète idéalement via variables d'env. pour projet pro)
+  authDomain: "vinted-entreprise.firebaseapp.com",
+  projectId: "vinted-entreprise",
+  storageBucket: "vinted-entreprise.appspot.com", // Vérifiez ce nom dans votre console Firebase (généralement sans firebase)
+  messagingSenderId: "301194568056",
+  appId: "1:301194568056:web:ef6da7eb1c0795e2eae88d",
+  measurementId: "G-EEGS88XHHB" // Optionnel pour Analytics
 };
